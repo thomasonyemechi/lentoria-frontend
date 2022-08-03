9da584fc61f320d1a21dd28a74894ff94b9455e9
thomasonyemechi <thomasonyemechi03@gmail.com>
2022-08-03 11:40:58 +0100

    ip

--- resources/views/layouts/instructor.blade.php
+++ resources/views/layouts/instructor.blade.php
@@ -26,7 +26,6 @@
 
 <body>
 
-<<<<<<< HEAD
     <div class="littleAlert"></div>
     @include('layouts.alert_top')
     @include('layouts.nav')
@@ -68,53 +67,6 @@
     <script src="{{ asset('assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
     <script src="{{ asset('assets/cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.12/clipboard.min.js') }}"></script>
     <script src="{{ asset('assets/js/theme.min.js') }}"></script>
-=======
-	@include('layouts.nav')
-
-	@yield('page_content')
-
-	@include('layouts.instructors_footer')
-
-	{{-- Javascripts Here .... --}}
-
-
-	<script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/odometer/odometer.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/flatpickr/dist/flatpickr.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/inputmask/dist/jquery.inputmask.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/quill/dist/quill.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/file-upload-with-preview/dist/file-upload-with-preview.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/dragula/dist/dragula.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/bs-stepper/dist/js/bs-stepper.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/dropzone/dist/min/dropzone.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/jQuery.print/jQuery.print.js') }}"></script>
-	<script src="{{ asset('assets/libs/prismjs/prism.js') }}"></script>
-	<script src="{{ asset('assets/libs/prismjs/components/prism-scss.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/%40yaireo/tagify/dist/tagify.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/tiny-slider/dist/min/tiny-slider.js') }}"></script>
-	<script src="{{ asset('assets/libs/%40popperjs/core/dist/umd/popper.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/tippy.js/dist/tippy-bundle.umd.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/typed.js/lib/typed.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/jsvectormap/dist/js/jsvectormap.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/jsvectormap/dist/maps/world.js') }}"></script>
-	<script src="{{ asset('assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
-	<script src="{{ asset('assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
-	<!-- clipboard -->
-	<script src="{{ asset('assets/cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.12/clipboard.min.js') }}"></script>
-	<!-- Theme JS -->
-	<script src="{{ asset('assets/js/theme.min.js') }}"></script>
-	@method('js')
-
-
-
-
->>>>>>> 887db5afd21b9acc3d10038192eb9bef621f9a9f
 </body>
 
 </html>
