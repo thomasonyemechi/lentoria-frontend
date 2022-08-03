dcd421ad37728863e36857226d2e8197773b705a
T3mmyKay <103121606+T3mmyKay@users.noreply.github.com>
2022-08-03 05:29:39 +0100

    changes

--- resources/views/instructor/courses.blade.php
+++ resources/views/instructor/courses.blade.php
@@ -1,120 +1,113 @@
 @extends('layouts.instructor')
 @section('page_title')
-    Instructors Dashboard
+	Instructors Dashboard
 @endsection
 
 @section('page_content')
-    <div class="pt-5 pb-5">
-        <div class="container">
-            {!! instructorInfo() !!}
+	<div class="pt-5 pb-5">
+		<div class="container">
+			{!! instructorInfo() !!}
 
-        
 
-            <div class="row mt-0 mt-md-4">
-                <div class="col-lg-3 col-md-4 col-12">
-                    @include('layouts.sidebar')
-                </div>
-                <div class="col-lg-9 col-md-8 col-12">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <h3 class="mb-0">Courses</h3>
-                            <span>Manage your courses and its update like live, draft and
-                                insight.</span>
-                        </div>
-                        <div class="card-body">
-                            <form class="row">
-                                <div class="col-lg-9 col-md-7 col-12 mb-lg-0 mb-2">
-                                    <input type="search" class="form-control" placeholder="Search Your Courses" />
-                                </div>
-                                <div class="col-lg-3 col-md-5 col-12">
-                                    <select class="selectpicker" data-width="100%">
-                                        <option value="">Date Created</option>
-                                        <option value="Newest">Newest</option>
-                                        <option value="High Rated">High Rated</option>
-                                        <option value="Law Rated">Law Rated</option>
-                                        <option value="High Earned">High Earned</option>
-                                    </select>
-                                </div>
-                            </form>
-                        </div>
-                        <!-- Table -->
-                        <div class="table-responsive border-0 overflow-y-hidden">
-                            <table class="table mb-0 text-nowrap">
-                                <thead class="table-light">
-                                    <tr>
-                                        <th scope="col" class="border-0">Courses</th>
-                                        <th scope="col" class="border-0">Students</th>
-                                        <th scope="col" class="border-0">Rating</th>
-                                        <th scope="col" class="border-0">Status</th>
-                                        <th scope="col" class="border-0"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td class="border-top-0">
-                                            <div class="d-lg-flex">
-                                                <div>
-                                                    <a href="#">
-                                                        <img src="{{ asset('assets/images/course/course-wordpress.jpg') }}"
-                                                            alt="" class="rounded img-4by3-lg" /></a>
-                                                </div>
-                                                <div class="ms-lg-3 mt-2 mt-lg-0">
-                                                    <h4 class="mb-1 h5">
-                                                        <a href="#" class="text-inherit">
-                                                            Create a Website with WordPress
-                                                        </a>
-                                                    </h4>
-                                                    <ul class="list-inline fs-6 mb-0">
-                                                        <li class="list-inline-item">
-                                                            <i class="far fa-clock me-1"></i>1h 30m
-                                                        </li>
-                                                        <li class="list-inline-item">
-                                                            <svg class="me-1 mt-n1" width="16" height="16"
-                                                                viewBox="0 0 16 16" fill="none"
-                                                                xmlns="http://www.w3.org/2000/svg">
-                                                                <rect x="3" y="8" width="2"
-                                                                    height="6" rx="1" fill="#754FFE"></rect>
-                                                                <rect x="7" y="5" width="2"
-                                                                    height="9" rx="1" fill="#DBD8E9"></rect>
-                                                                <rect x="11" y="2" width="2"
-                                                                    height="12" rx="1" fill="#DBD8E9">
-                                                                </rect>
-                                                            </svg>Beginner
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td class="border-top-0">11,200</td>
-                                        <td class="border-top-0">
-                                            <span class="text-warning">4.5<i class="mdi mdi-star"></i></span>(3,250)
-                                        </td>
-                                        <td class="border-top-0">
-                                            <span class="badge bg-success">Live</span>
-                                        </td>
-                                        <td class="text-muted border-top-0">
-                                            <span class="dropdown dropstart">
-                                                <a class="text-muted text-decoration-none" href="#" role="button"
-                                                    id="courseDropdown" data-bs-toggle="dropdown" data-bs-offset="-20,20"
-                                                    aria-expanded="false">
-                                                    <i class="fe fe-more-vertical"></i>
-                                                </a>
-                                                <span class="dropdown-menu" aria-labelledby="courseDropdown">
-                                                    <span class="dropdown-header">Setting </span>
-                                                    <a class="dropdown-item" href="#"><i
-                                                            class="fe fe-edit dropdown-item-icon"></i>Edit</a>
-                                                    <a class="dropdown-item" href="#"><i
-                                                            class="fe fe-trash dropdown-item-icon"></i>Remove</a>
-                                                </span>
-                                            </span>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+			<div class="row mt-0 mt-md-4">
+				<div class="col-lg-3 col-md-4 col-12">
+					@include('layouts.sidebar')
+				</div>
+				<div class="col-lg-9 col-md-8 col-12">
+					<div class="card mb-4">
+						<div class="card-header">
+							<h3 class="mb-0">Courses</h3>
+							<span>Manage your courses and its update like live, draft and
+								insight.</span>
+						</div>
+						<div class="card-body">
+							<form class="row">
+								<div class="col-lg-9 col-md-7 col-12 mb-lg-0 mb-2">
+									<input type="search" class="form-control" placeholder="Search Your Courses" />
+								</div>
+								<div class="col-lg-3 col-md-5 col-12">
+									<select class="selectpicker" data-width="100%">
+										<option value="">Date Created</option>
+										<option value="Newest">Newest</option>
+										<option value="High Rated">High Rated</option>
+										<option value="Law Rated">Law Rated</option>
+										<option value="High Earned">High Earned</option>
+									</select>
+								</div>
+							</form>
+						</div>
+						<!-- Table -->
+						<div class="table-responsive border-0 overflow-y-hidden">
+							<table class="table mb-0 text-nowrap">
+								<thead class="table-light">
+									<tr>
+										<th scope="col" class="border-0">Courses</th>
+										<th scope="col" class="border-0">Students</th>
+										<th scope="col" class="border-0">Rating</th>
+										<th scope="col" class="border-0">Status</th>
+										<th scope="col" class="border-0"></th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td class="border-top-0">
+											<div class="d-lg-flex">
+												<div>
+													<a href="#">
+														<img src="{{ asset('assets/images/course/course-wordpress.jpg') }}" alt=""
+															class="rounded img-4by3-lg" /></a>
+												</div>
+												<div class="ms-lg-3 mt-2 mt-lg-0">
+													<h4 class="mb-1 h5">
+														<a href="#" class="text-inherit">
+															Create a Website with WordPress
+														</a>
+													</h4>
+													<ul class="list-inline fs-6 mb-0">
+														<li class="list-inline-item">
+															<i class="far fa-clock me-1"></i>1h 30m
+														</li>
+														<li class="list-inline-item">
+															<svg class="me-1 mt-n1" width="16" height="16" viewBox="0 0 16 16" fill="none"
+																xmlns="http://www.w3.org/2000/svg">
+																<rect x="3" y="8" width="2" height="6" rx="1" fill="#754FFE"></rect>
+																<rect x="7" y="5" width="2" height="9" rx="1" fill="#DBD8E9"></rect>
+																<rect x="11" y="2" width="2" height="12" rx="1" fill="#DBD8E9">
+																</rect>
+															</svg>Beginner
+														</li>
+													</ul>
+												</div>
+											</div>
+										</td>
+										<td class="border-top-0">11,200</td>
+										<td class="border-top-0">
+											<span class="text-warning">4.5<i class="mdi mdi-star"></i></span>(3,250)
+										</td>
+										<td class="border-top-0">
+											<span class="badge bg-success">Live</span>
+										</td>
+										<td class="text-muted border-top-0">
+											<span class="dropdown dropstart">
+												<a class="text-muted text-decoration-none" href="#" role="button" id="courseDropdown"
+													data-bs-toggle="dropdown" data-bs-offset="-20,20" aria-expanded="false">
+													<i class="fe fe-more-vertical"></i>
+												</a>
+												<span class="dropdown-menu" aria-labelledby="courseDropdown">
+													<span class="dropdown-header">Setting </span>
+													<a class="dropdown-item" href="#"><i class="fe fe-edit dropdown-item-icon"></i>Edit</a>
+													<a class="dropdown-item" href="#"><i class="fe fe-trash dropdown-item-icon"></i>Remove</a>
+												</span>
+											</span>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
--- resources/views/layouts/instructor.blade.php
+++ resources/views/layouts/instructor.blade.php
@@ -2,82 +2,84 @@
 <html lang="en">
 
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<title>@yield('page_title')</title>
-<link href="{{ asset('assets/fonts/feather/feather.css')}}" rel="stylesheet" />
-<link href="{{ asset('assets/libs/bootstrap-icons/font/bootstrap-icons.css')}}" rel="stylesheet" />
-<link href="{{ asset('assets/libs/dragula/dist/dragula.min.css')}}" rel="stylesheet" />
-<link href="{{ asset('assets/libs/%40mdi/font/css/materialdesignicons.min.css')}}" rel="stylesheet" />
-<link href="{{ asset('assets/libs/prismjs/themes/prism.css')}}" rel="stylesheet" />
-<link href="{{ asset('assets/libs/dropzone/dist/dropzone.css')}}" rel="stylesheet" />
-<link href="{{ asset('assets/libs/magnific-popup/dist/magnific-popup.css')}}" rel="stylesheet" />
-<link href="{{ asset('assets/libs/bootstrap-select/dist/css/bootstrap-select.min.css')}}" rel="stylesheet">
-<link href="{{ asset('assets/libs/%40yaireo/tagify/dist/tagify.css')}}" rel="stylesheet">
-<link href="{{ asset('assets/libs/tiny-slider/dist/tiny-slider.css')}}" rel="stylesheet">
-<link href="{{ asset('assets/libs/tippy.js/dist/tippy.css')}}" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('assets/cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css') }}">
-<script src="{{ asset('assets/libs/jquery/dist/jquery.min.js')}}"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>@yield('page_title')</title>
+	<link href="{{ asset('assets/fonts/feather/feather.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/libs/bootstrap-icons/font/bootstrap-icons.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/libs/dragula/dist/dragula.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/libs/%40mdi/font/css/materialdesignicons.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/libs/prismjs/themes/prism.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/libs/dropzone/dist/dropzone.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/libs/magnific-popup/dist/magnific-popup.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/libs/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/libs/%40yaireo/tagify/dist/tagify.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/libs/tiny-slider/dist/tiny-slider.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/libs/tippy.js/dist/tippy.css') }}" rel="stylesheet">
+	<link rel="stylesheet" href="{{ asset('assets/cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/css/theme.min.css') }}">
+
+	<script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
 
 
 
-<link rel="stylesheet" href="{{ asset('assets/css/theme.min.css')}}">
 
 
-<script src="{{ asset('assets/js/general.js') }}"></script>
+	<script src="{{ asset('assets/js/general.js') }}"></script>
 
 </head>
 
 <body>
 
-        @include('layouts.nav')
+	@include('layouts.nav')
 
-        @yield('page_content')
+	@yield('page_content')
 
-@include('layouts.instructors_footer')
+	@include('layouts.instructors_footer')
 
-{{-- Javascripts Here .... --}}
-
-<script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{ asset('assets/libs/odometer/odometer.min.js')}}"></script>
-<script src="{{ asset('assets/libs/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
-<script src="{{ asset('assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{ asset('assets/libs/bootstrap-select/dist/js/bootstrap-select.min.js')}}"></script>
-<script src="{{ asset('assets/libs/flatpickr/dist/flatpickr.min.js')}}"></script>
-<script src="{{ asset('assets/libs/inputmask/dist/jquery.inputmask.min.js')}}"></script>
-<script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js')}}"></script>
-<script src="{{ asset('assets/libs/quill/dist/quill.min.js')}}"></script>
-<script src="{{ asset('assets/libs/file-upload-with-preview/dist/file-upload-with-preview.min.js')}}"></script>
-<script src="{{ asset('assets/libs/dragula/dist/dragula.min.js')}}"></script>
-<script src="{{ asset('assets/libs/bs-stepper/dist/js/bs-stepper.min.js')}}"></script>
-<script src="{{ asset('assets/libs/dropzone/dist/min/dropzone.min.js')}}"></script>
-<script src="{{ asset('assets/libs/jQuery.print/jQuery.print.js')}}"></script>
-<script src="{{ asset('assets/libs/prismjs/prism.js')}}"></script>
-<script src="{{ asset('assets/libs/prismjs/components/prism-scss.min.js')}}"></script>
-<script src="{{ asset('assets/libs/%40yaireo/tagify/dist/tagify.min.js')}}"></script>
-<script src="{{ asset('assets/libs/tiny-slider/dist/min/tiny-slider.js')}}"></script>
-<script src="{{ asset('assets/libs/%40popperjs/core/dist/umd/popper.min.js')}}"></script>
-<script src="{{ asset('assets/libs/tippy.js/dist/tippy-bundle.umd.min.js')}}"></script>
-<script src="{{ asset('assets/libs/typed.js/lib/typed.min.js')}}"></script>
-<script src="{{ asset('assets/libs/jsvectormap/dist/js/jsvectormap.min.js')}}"></script>
-<script src="{{ asset('assets/libs/jsvectormap/dist/maps/world.js')}}"></script>
-<script src="{{ asset('assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{ asset('assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js')}}"></script>
-<script src="{{ asset('assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{ asset('assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js')}}"></script>
-
-@method('js')
+	{{-- Javascripts Here .... --}}
 
 
+	<script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/odometer/odometer.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/flatpickr/dist/flatpickr.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/inputmask/dist/jquery.inputmask.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/quill/dist/quill.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/file-upload-with-preview/dist/file-upload-with-preview.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/dragula/dist/dragula.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/bs-stepper/dist/js/bs-stepper.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/dropzone/dist/min/dropzone.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/jQuery.print/jQuery.print.js') }}"></script>
+	<script src="{{ asset('assets/libs/prismjs/prism.js') }}"></script>
+	<script src="{{ asset('assets/libs/prismjs/components/prism-scss.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/%40yaireo/tagify/dist/tagify.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/tiny-slider/dist/min/tiny-slider.js') }}"></script>
+	<script src="{{ asset('assets/libs/%40popperjs/core/dist/umd/popper.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/tippy.js/dist/tippy-bundle.umd.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/typed.js/lib/typed.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/jsvectormap/dist/js/jsvectormap.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/jsvectormap/dist/maps/world.js') }}"></script>
+	<script src="{{ asset('assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+	<script src="{{ asset('assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
+
+	@method('js')
 
 
-<!-- clipboard -->
-<script src="{{ asset('assets/cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.12/clipboard.min.js') }}"></script>
 
 
-<!-- Theme JS -->
-<script src="{{ asset('assets/js/theme.min.js')}}"></script>
+	<!-- clipboard -->
+	<script src="{{ asset('assets/cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.12/clipboard.min.js') }}"></script>
+
+
+	<!-- Theme JS -->
+	<script src="{{ asset('assets/js/theme.min.js') }}"></script>
 </body>
 
 </html>
