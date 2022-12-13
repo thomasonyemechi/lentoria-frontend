c236bf847e2c5ecb1b08b18e338a7a32b66070bf
thomasonyemechi <thomasonyemechi03@gmail.com>
2022-12-13 09:17:26 +0100

    admin

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -1,6 +1,11 @@
-api_url = 'http://api.lentoria.com/api/';
-api_root = 'http://api.lentoria.com/';
-image_url = 'http://api.lentoria.com/assets/uploads/';
+// api_url = 'http://api.lentoria.com/api/';
+// api_root = 'http://api.lentoria.com/';
+// image_url = 'http://api.lentoria.com/assets/uploads/';
+// video_url = "https://lentoria.site/watchvideo/";
+
+api_url = 'http://127.0.0.1:8000/api/';
+api_root = 'http://127.0.0.1:8000/';
+image_url = 'http://127.0.0.1:8000/assets/uploads/';
 video_url = "https://lentoria.site/watchvideo/";
 
 function validateEmail(email) {
@@ -273,4 +278,10 @@ function getWithExpiry(key) {
         return null
     }
     return item.value
+}
+
+
+function errorFetchingDataRefresh()
+{
+    return `An error occured while fetching your data <a href="">Refresh Page</a>`
 }
